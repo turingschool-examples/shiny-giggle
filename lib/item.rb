e6d5b8dad6a604f66e2e59435ebe897e8e3712fd
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2024-11-12 18:51:03 +0100

    Add add_bid method to Item class to handle bids

--- lib/item.rb
+++ lib/item.rb
@@ -1,12 +1,12 @@
-
+class Item
   attr_reader :name, :bids
 
   def initialize(name)
-    @name = name
-    @bids = {}
+    @name = name        # The name of the item being auctioned
+    @bids = {}          # A hash to store bids where keys are attendees and values are bid amounts
   end
 
   def add_bid(attendee, amount)
-    @bids[attendee] = amount
+    @bids[attendee] = amount # Add the attendee as a key and their bid amount as the value to the bids hash
   end
 end
