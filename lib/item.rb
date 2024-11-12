e2d5e836451b491bf5528734bb32bc6d31ddca03
Will Fox <william.macdonald.fox@gmail.com>
2024-11-12 13:57:38 -0500

    iteration 2

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,16 @@
-class Items
-    attr_reader :name
+class Item
+    attr_reader :name, :bids
      
     def initialize(name)
-        @name = "Chalkware Piggy Bank"
+        @name = name
+        @bids = {}
+    end
+
+    def add_bid(attendee, amount)
+        @bids[attendee] = amount
+    end
+
+    def current_high_bid
+      @bids.values.max
     end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,9 +1,13 @@
 require './lib/item'
+require './lib/attendee'
 
 RSpec.describe Item do 
     before(:each) do
         @item1 = Item.new('Chalkware Piggy Bank')
         @item2 = Item.new('Bamboo Picture Frame')
+
+        @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+        @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
     end
 
     it 'exists' do
@@ -13,5 +17,20 @@ RSpec.describe Item do
     it 'has attributes' do
         expect(@item1.name).to eq("Chalkware Piggy Bank")
         expect(@item2.name).to eq("Bamboo Picture Frame")
+        expect(@item1.bids).to eq({})
+    end
+
+    it 'can add a bid to an item' do
+        @item1.add_bid(@attendee1, 20)
+        @item1.add_bid(@attendee2, 22)
+
+        expect(@item1.bids).to eq({@attendee1 => 20, @attendee2 => 22})
+    end
+
+    it 'can return the highest current bid' do
+        @item1.add_bid(@attendee1, 20)
+        @item1.add_bid(@attendee2, 22)
+
+        expect(@item1.current_high_bid).to eq(22)
     end
 end
