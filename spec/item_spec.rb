a1cf04cb66d1fba2a47cb82411d29e60dff197f4
Dustin Peukert <dpeukert04@gmail.com>
2024-11-12 11:28:04 -0500

    add test for #add_bid

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Item do
     before(:each) do
         @item1 = Item.new('Chalkware Piggy Bank')
         @item2 = Item.new('Bamboo Picture Frame')
+        @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+        @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
     end
 
     it 'exists' do
@@ -15,5 +17,16 @@ RSpec.describe Item do
         expect(@item2.name).to eq("Bamboo Picture Frame")
         expect(@item1.bids).to eq({})
         expect(@item2.bids).to eq({})
+    end
+
+    describe '#add_bid' do
+        it 'adds a bid to an item from a specific person' do
+            expect(@item1.bids).to eq({})
+
+            @item1.add_bid(@attendee2, 20)
+            @item1.add_bid(@attendee1, 22)
+
+            expect(@item1.bids).to eq({@attendee2=>20, @attendee1=>22})
+        end
     end
 end
