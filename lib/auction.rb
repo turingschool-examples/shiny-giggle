542aac278a0a9a85b0f15218fe837d6d91f0777a
Kristin Weiland <kristinweiland@gmail.com>
2024-11-12 09:00:12 -0800

    Debug existing code for the attendee and auction classes, and add unpopular_items and potential_revenues methods to auction class and their tests.

--- lib/auction.rb
+++ lib/auction.rb
@@ -6,10 +6,34 @@ class Auction
     end
 
     def add_item(item)
-        @items.unshift(item)
+        @items << item
     end
 
     def item_names
-        raise NotImplementedError
+        item_names = []
+        @items.each do |item|
+            item_names << item.name
+        end
+        item_names
+    end
+
+    def unpopular_items
+        no_bid_items = []
+        @items.each do |item| #go thru each item
+            if item.bids.count == 0 #check if its bids hash has any key:value pairs
+                no_bid_items << item
+            end
+        end
+        no_bid_items
+    end
+
+    def potential_revenues
+        revenue_sum = 0
+        @items.each do |item| #go thru each item
+            if item.bids.count > 0 #make sure it has a bid
+                revenue_sum += item.current_high_bid #add its highes bid to the total
+            end
+        end
+        revenue_sum
     end
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,10 +1,20 @@
+require 'pry'
 require './lib/item'
+require './lib/attendee'
 require './lib/auction'
 
 RSpec.describe Auction do
     before(:each) do 
         @item1 = Item.new('Chalkware Piggy Bank')
         @item2 = Item.new('Bamboo Picture Frame')
+        @item3 = Item.new('Homemade Chocolate Chip Cookies')
+        @item4 = Item.new('2 Days Dogsitting')
+        @item5 = Item.new('Forever Stamps')
+
+        @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+        @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+        @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
+
         @auction = Auction.new
     end
 
@@ -28,5 +38,35 @@ RSpec.describe Auction do
         @auction.add_item(@item2)
 
         expect(@auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+    end
+
+    it '#unpopular_items' do 
+        @auction.add_item(@item1)
+        @auction.add_item(@item2)
+        @auction.add_item(@item3)
+        @auction.add_item(@item4)
+        @auction.add_item(@item5)
+
+        @item1.add_bid(@attendee2, 20)
+        @item1.add_bid(@attendee1, 22)
+        @item4.add_bid(@attendee3, 50)
+        @item3.add_bid(@attendee2, 15)
+
+        expect(@auction.unpopular_items).to eq([@item2, @item5])
+    end
+
+    it '#potential_revenue' do 
+        @auction.add_item(@item1)
+        @auction.add_item(@item2)
+        @auction.add_item(@item3)
+        @auction.add_item(@item4)
+        @auction.add_item(@item5)
+
+        @item1.add_bid(@attendee2, 20)
+        @item1.add_bid(@attendee1, 22)
+        @item4.add_bid(@attendee3, 50)
+        @item3.add_bid(@attendee2, 15)
+
+        expect(@auction.potential_revenues).to eq(87)
     end
 end
