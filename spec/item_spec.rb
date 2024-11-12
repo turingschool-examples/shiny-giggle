3b3ad0a6a205738e2268e8d9f4992d63853647f9
Kevin Newland <kevin.newland@yahoo.com>
2024-11-12 12:55:33 -0600

    add test for bidding closed

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe Item do
         expect(@item1.name).to eq("Chalkware Piggy Bank")
         expect(@item2.name).to eq("Bamboo Picture Frame")
     end
+
+    it 'will not add bid if bidding is closed' do
+        attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+        @item1.add_bid(attendee1, 22)
+        expect(@item1.bids).to eq({attendee1 => 22})
+
+        @item1.close_bidding
+
+        attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+        @item1.add_bid(attendee2, 20)
+        expect(@item1.bids).to eq({attendee1 => 22}) #shows it cant be added due to bidding closed
+    end
 end
