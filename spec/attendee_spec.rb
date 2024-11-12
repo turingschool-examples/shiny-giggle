9125c7d428cc2b26654d6e7351d15a31e0e8ec1a
Jillian Bleggi <jillian.bleggi@gmail.com>
2024-11-12 11:21:29 -0500

    Update Attendee spec with It2 interaction pattern examples

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -2,15 +2,24 @@ require './lib/attendee'
 
 RSpec.describe Attendee do
     before(:each) do
-        @attendee = Attendee.new({name: 'Megan', budget: '$50'})
+        @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+        @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+        @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
     end
 
     it 'exists' do
-        expect(@attendee).to be_a(Attendee)
+        expect(@attendee1).to be_a(Attendee)
     end
 
-    it 'has attributes' do
-        expect(@attendee.name).to eq('Megan')
-        expect(@attendee.budget).to eq(50)
+    it 'has name attributes' do
+        expect(@attendee1.name).to eq('Megan')
+        expect(@attendee2.name).to eq('Bob')
+        expect(@attendee3.name).to eq('Mike')
+    end
+    
+    it 'has budget attributes' do
+        expect(@attendee1.budget).to eq(50)
+        expect(@attendee2.budget).to eq(75)
+        expect(@attendee3.budget).to eq(100)
     end
 end
