5cb47431780e7a610de50b93d35986d1d8ee4c73
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2024-11-12 18:48:13 +0100

    Format gems

--- Gemfile
+++ Gemfile
@@ -1,4 +1,10 @@
-source 'https://rubygems.org'
+source 'https://rubygems.org' 
 
 gem 'rspec'
+gem 'rubocop'
+gem 'rubocop-packaging'
+gem 'rubocop-performance'
+gem 'rubocop-rspec'
+gem 'rubocop-shopify'
+gem 'ruby-lsp'
 gem 'simplecov', require: false
--- lib/item.rb
+++ lib/item.rb
@@ -1,8 +1,12 @@
-class Item 
-    attr_reader :name, :bids
-     
-    def initialize(name)
-        @name = name
-        @bids = {}
-    end
+
+  attr_reader :name, :bids
+
+  def initialize(name)
+    @name = name
+    @bids = {}
+  end
+
+  def add_bid(attendee, amount)
+    @bids[attendee] = amount
+  end
 end
