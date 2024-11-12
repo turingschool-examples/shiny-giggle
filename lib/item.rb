e2150f35f74983d2e1ec5340a24e7ab10d440f28
Kevin Newland <kevin.newland@yahoo.com>
2024-11-12 12:54:59 -0600

    add methods for item

--- lib/item.rb
+++ lib/item.rb
@@ -1,7 +1,23 @@
-class Items
-    attr_reader :name
+class Item
+    attr_reader :name, :bids, :bidding_is_closed
      
     def initialize(name)
-        @name = "Chalkware Piggy Bank"
+        @name = name
+        @bids = {}
+        @bidding_is_closed = false
+    end
+
+    def add_bid(attendee, amount)
+        if !@bidding_is_closed 
+          @bids[attendee] = amount
+        end
+    end
+
+    def current_high_bid #need largest number from the values in the hash
+      @bids.values.max  
+    end
+
+    def close_bidding
+      @bidding_is_closed = true
     end
 end
