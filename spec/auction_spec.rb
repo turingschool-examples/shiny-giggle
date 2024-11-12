85ca5b2d2e57764c775496cc1f830b2160eacda1
elysableu <elysabward@gmail.com>
2024-11-12 09:18:56 -0700

    Update before(:each) block

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -5,6 +5,13 @@ RSpec.describe Auction do
     before(:each) do 
         @item1 = Item.new('Chalkware Piggy Bank')
         @item2 = Item.new('Bamboo Picture Frame')
+        @item3 = Item.new('Homemade Chocolate Chip Cookies')
+        @item4 = Item.new('2 Days Dogsitting')
+        @item5 = Item.new('Forever Stamps')
+
+        @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+        @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
+        @attendee3 = Attendee.new({name: 'Mike', budget: '$100'})
         @auction = Auction.new
     end
 
