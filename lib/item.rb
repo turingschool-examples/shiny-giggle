e2eb80d80e1f74636fb46881022d868ee60be31b
Dustin Peukert <dpeukert04@gmail.com>
2024-11-12 12:07:49 -0500

    write #close_bidding method; add logic to #add_bid

--- lib/item.rb
+++ lib/item.rb
@@ -1,18 +1,29 @@
 class Item
-    attr_reader :name, :bids
+    attr_reader :name, :bids, :closed
      
     def initialize(name)
         @name = name
         @bids = {}
+        @closed = false
+    end
+    
+    def closed?
+        @closed
     end
 
     def add_bid(attendee, bid)
-        @bids[attendee] = bid
+        if @closed == false
+            @bids[attendee] = bid
+        end
     end
 
     def current_high_bid
         @bids.max_by do |attendee, bid|
             bid
         end[1]
+    end
+
+    def close_bidding
+        @closed = true
     end
 end
