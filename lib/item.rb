3e518f5d088bd6e4ccedfddc50ec2bdf94f59469
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2024-11-12 19:18:56 +0100

    Add current_high_bid method to Item class

--- lib/item.rb
+++ lib/item.rb
@@ -10,4 +10,8 @@ class Item
   def add_bid(attendee, amount)
     @bids[attendee] = amount # Add the attendee as a key and their bid amount as the value to the bids hash
   end
+    # Returns the highest bid amount for this item
+  def current_high_bid
+    @bids.values.max
+  end
 end
