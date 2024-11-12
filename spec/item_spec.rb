18d6f57b1549d8b45268ed2b22b6e25e3e653a9e
elysableu <elysabward@gmail.com>
2024-11-12 09:15:37 -0700

    Add tests for item bids

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,9 +1,13 @@
 require './lib/item'
+require './lib/attendee'
 
 RSpec.describe Item do 
     before(:each) do
         @item1 = Item.new('Chalkware Piggy Bank')
         @item2 = Item.new('Bamboo Picture Frame')
+
+        @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+        @attendee2 = Attendee.new({name: 'Bob', budget: '$75'})
     end
 
     it 'exists' do
@@ -14,4 +18,22 @@ RSpec.describe Item do
         expect(@item1.name).to eq("Chalkware Piggy Bank")
         expect(@item2.name).to eq("Bamboo Picture Frame")
     end
+
+    it 'has a list of bids defaulted to an empty hash' do
+        expect(@item1.bids).to eq({})
+    end
+
+    it 'can be bid on' do
+        expect(@item1.bids).to eq({})
+
+        @item1.add_bid(@attendee2, 20)
+        @item1.add_bid(@attendee1, 22)
+
+        expect(@item1.bids).to eq({
+            @attendee2 => 20,
+            @attendee1 => 22
+        })
+    end
+
+   
 end
